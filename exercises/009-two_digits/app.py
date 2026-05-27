39160c24aed329bb5965368c9dec9cb60b8e91ea
Daniel Anderi <100786824+danderi@users.noreply.github.com>
2026-05-27 19:53:18 +0000

    excercise 9 done

--- exercises/009-two_digits/app.py
+++ exercises/009-two_digits/app.py
@@ -1,7 +1,7 @@
 # Complete the function to return the tens digit and the units digit of any interger
 def two_digits(number):
   # Your code here
-  return None
+    return int(str(number)[0]), int(str(number)[1])
    
 
 # Invoke the function with any two digit integer as its argument
