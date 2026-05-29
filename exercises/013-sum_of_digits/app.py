3a8c458a4c77d166dd6d6cbb845c112060d1fd1c
Daniel Anderi <100786824+danderi@users.noreply.github.com>
2026-05-29 19:47:32 +0000

    Completed up to exercise 16

--- exercises/013-sum_of_digits/app.py
+++ exercises/013-sum_of_digits/app.py
@@ -1,6 +1,11 @@
 # Complete the function "digits_sum" so that it prints the sum of a three-digit number
 def digits_sum(num):
-  return None
+  aux = str(num)
+  number = 0
+  for i in aux:
+    number += int(i)
+
+  return number
 
 
 # Invoke the function with any three-digit number
--- exercises/016-century/app.py
+++ exercises/016-century/app.py
@@ -1,7 +1,12 @@
 # Complete the function to return the respective number of the century
+import math
 def century(year):
-  return None
+  aux = 0
+  aux2 = year%100
+  if aux2 > 0:
+    aux = 1
+  return round(year/100)+aux
 
 
 # Invoke the function with any given year
-print(century())
+print(century(2001))
