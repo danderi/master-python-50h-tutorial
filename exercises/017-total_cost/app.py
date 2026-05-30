7914165fe87b749a2590cd5686ffba21d9b0d08e
Daniel Anderi <100786824+danderi@users.noreply.github.com>
2026-05-30 21:28:22 +0000

    completed up to 18

--- exercises/017-total_cost/app.py
+++ exercises/017-total_cost/app.py
@@ -1,6 +1,12 @@
 # Complete the function to return the total cost in dollars and cents of (n) cupcakes
 def total_cost(d, c, n):
-    return None
+    cents = c * n
+    dollars = d * n
+    if cents > 99:
+        dollars += round(cents / 100)
+        cents = cents % 100
+
+    return dollars, cents
 
 
 # Invoke the function with three integers: total_cost(dollars, cents, number_of_cupcakes)
--- exercises/018-day_of_week/app.py
+++ exercises/018-day_of_week/app.py
@@ -1,7 +1,9 @@
 # Complete the function to return the number of day of the week for k'th day of year
 def day_of_week(k):
-  return None
+  day = k%7
+  real_day = [None,4,5,6,0,1,2,3]
+  return real_day[k%7]
 
 
 # Invoke function day_of_week with an integer between 1 and 365
-print(day_of_week())
+print(day_of_week(1))
