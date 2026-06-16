07ac21aa428e6c4db70c9007275b789a268f5fb0
Daniel Anderi <100786824+danderi@users.noreply.github.com>
2026-06-16 21:49:29 +0000

    refactor exercise 31

--- exercises/031-sum-eigth-digit/app.py
+++ exercises/031-sum-eigth-digit/app.py
@@ -1,10 +1,6 @@
 # Your code here
 def all_digits_even(numbers):
-    even_num = []
-    for i in numbers:
-        if i >= 1000 and i <= 3000 and i%2 == 0:
-            even_num.append(i)
-
+    even_num = [i for i in numbers if i >= 1000 and i <= 3000 and i%2 == 0]
     return ", ".join(str(i) for i in even_num)
 
 print(all_digits_even([1000, 542, 1002, 3000, 4000, 2999, 2000]))
