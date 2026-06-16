63f74017186e32552ad36bb2e77a50f29b08d3b3
Daniel Anderi <100786824+danderi@users.noreply.github.com>
2026-06-16 21:44:28 +0000

    completed up to exercises 30 and 31

--- exercises/030-divisable-binary/app.py
+++ exercises/030-divisable-binary/app.py
@@ -1,1 +1,15 @@
 # Your code here
+def divisible_binary(binary_string):
+    binary_list = binary_string.split(",")
+    
+    result = []
+    for binary in binary_list:
+        # acá convertimos el binario a un entero base 2 usando la funcionalidad de int()
+        decimal = int(binary, 2)
+        
+        if decimal % 5 == 0:
+            result.append(binary)
+            
+    return ",".join(result)
+
+print(divisible_binary("0100,0011,1010,1001"))
--- exercises/031-sum-eigth-digit/app.py
+++ exercises/031-sum-eigth-digit/app.py
@@ -1,1 +1,10 @@
 # Your code here
+def all_digits_even(numbers):
+    even_num = []
+    for i in numbers:
+        if i >= 1000 and i <= 3000 and i%2 == 0:
+            even_num.append(i)
+
+    return ", ".join(str(i) for i in even_num)
+
+print(all_digits_even([1000, 542, 1002, 3000, 4000, 2999, 2000]))
