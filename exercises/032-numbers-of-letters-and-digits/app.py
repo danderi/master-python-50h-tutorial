b7296b697c4cf9c7190974d8d6f2b6b1ae82e3fc
Daniel Anderi <100786824+danderi@users.noreply.github.com>
2026-06-17 21:56:57 +0000

    completed up to exercise 032

--- exercises/032-numbers-of-letters-and-digits/app.py
+++ exercises/032-numbers-of-letters-and-digits/app.py
@@ -1,1 +1,14 @@
 # Your code here
+import string
+
+def letters_and_digits(frase):
+    elem = {"DIGITS": 0, "LETTERS" : 0}
+    for i in frase:
+        if i.isdigit():
+            elem["DIGITS"]+=1
+        elif i.isalpha():
+            elem["LETTERS"] += 1
+        
+    return f"LETTERS {elem['LETTERS']} DIGITS {elem['DIGITS']}"
+
+print(letters_and_digits("hello world! 123"))
