63c18e95fc8b3a9d72d3f1d957da2270e489ebe2
Daniel Anderi <100786824+danderi@users.noreply.github.com>
2026-07-01 01:55:42 +0000

    completed up to 33

--- exercises/033-number-of-uppercase/app.py
+++ exercises/033-number-of-uppercase/app.py
@@ -1,1 +1,12 @@
 # Your code here
+def number_of_uppercase(sentence):
+    elem = {"UPPERCASE":0, "LOWERCASE":0}
+    for i in sentence:
+        if i.islower():
+            elem["LOWERCASE"]+=1
+        elif i.isupper():
+            elem["UPPERCASE"]+=1
+
+    return f'UPPERCASE : {elem["UPPERCASE"]}\nLOWERCASE: {elem["LOWERCASE"]}'
+
+print(number_of_uppercase("Hello world!"))
