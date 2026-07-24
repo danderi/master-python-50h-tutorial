abe7f899c7d1d76921452bcbe9315367adfe4e28
Daniel Anderi <100786824+danderi@users.noreply.github.com>
2026-07-24 00:52:54 +0000

    exercise 35 completed

--- exercises/035-square-each-odd-number/app.py
+++ exercises/035-square-each-odd-number/app.py
@@ -1,1 +1,11 @@
 # Your code here
+def square_odd_numbers(str_num):
+    aux = []
+    for i in str_num:
+        if i == ",":
+            continue
+        elif int(i)%2 != 0:
+            aux.append(int(i)**2)
+    return aux
+
+print(square_odd_numbers("1,2,3,4,5,6,7,8,9"))
