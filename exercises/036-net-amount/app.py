bb37830dd1d7fee73efcfbb187aec6ec5e2396e8
Daniel Anderi <100786824+danderi@users.noreply.github.com>
2026-07-27 21:14:32 +0000

    Exercise 36 completed

--- exercises/036-net-amount/app.py
+++ exercises/036-net-amount/app.py
@@ -1,1 +1,17 @@
 # Your code here
+def net_amount(log):
+    amount = 0
+    transactions = log.split()
+    
+    for i in range(0, len(transactions), 2):
+        print(i)
+        action = transactions[i]
+        value = int(transactions[i + 1])
+
+        if action == "D":
+            amount += value
+        elif action == "W":
+            amount -= value
+
+    return amount
+print(net_amount("D 300 D 300 W 200 D 100"))  # Output: 150
